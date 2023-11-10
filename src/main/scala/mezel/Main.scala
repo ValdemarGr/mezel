dca43d623640465a06af9ce4ea3cf0eea5e73ab6
Valdemar Grange <randomvald0069@gmail.com>
2023-11-10 03:16:27 +0100

    work on astd

--- src/main/scala/mezel/Main.scala
+++ src/main/scala/mezel/Main.scala
@@ -6,6 +6,7 @@ import cats.effect.*
 import fs2.io.file.*
 import cats.parse.Parser as P
 import cats.parse.Parser0 as P0
+import _root_.io.circe.Json
 
 object Main extends IOApp.Simple {
 
@@ -45,3 +46,67 @@ object Main extends IOApp.Simple {
     //   .drain
   }
 }
+
+final case class SafeUri(value: String)
+
+final case class InitializeBuildParams(
+  rootUri: SafeUri,
+  displayName: String,
+  version: String,
+  bspVersion: String,
+  capabilities: BuildClientCapabilities
+)
+
+final case class BuildClientCapabilities(languageIds: List[String])
+
+final case class InitializeBuildResult(
+  displayName: String,
+  version: String,
+  bspVersion: String,
+  capabilities: BuildServerCapabilities
+)
+
+final case class AnyProvider(languageIds: List[String])
+
+final case class BuildServerCapabilities(
+  compileProvider: Option[AnyProvider],
+  testProvider: Option[AnyProvider],
+  runProvider: Option[AnyProvider],
+  debugProvider: Option[AnyProvider],
+  inverseSourcesProvider: Option[Boolean],
+  dependencySourcesProvider: Option[Boolean],
+  dependencyModulesProvider: Option[Boolean],
+  resourcesProvider: Option[Boolean],
+  outputPathsProvider: Option[Boolean],
+  buildTargetChangedProvider: Option[Boolean],
+  jvmRunEnvironmentProvider: Option[Boolean],
+  jvmTestEnvironmentProvider: Option[Boolean],
+  canReload: Option[Boolean]
+)
+
+final case class BuildTargetIdentifier(
+  uri: SafeUri
+)
+
+final case class BuildTargetCapabilities(
+  canCompile: Option[Boolean],
+  canTest: Option[Boolean],
+  canRun: Option[Boolean],
+  canDebug: Option[Boolean]
+)
+
+final case class BuildTarget(
+  id: BuildTargetIdentifier,
+  displayName: Option[String],
+  baseDirectory: Option[SafeUri],
+  tags: List[String],
+  languageIds: List[String],
+  dependencies: List[BuildTargetIdentifier],
+  capabilities: BuildTargetCapabilities,
+  dataKind: Option[String],
+  data: Option[Json]
+)
+
+final case class WorkspaceBuildTargetsResult(
+  targets: List[BuildTarget]
+)
