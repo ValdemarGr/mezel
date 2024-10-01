98b827cfb4b9fc545c3dcd7fa6b8ef877448f4cd
Valdemar Grange <randomvald0069@gmail.com>
2024-10-01 13:30:32 +0200

    fix: #43

--- src/main/scala/mezel/Label.scala
+++ src/main/scala/mezel/Label.scala
@@ -1,5 +1,6 @@
 package mezel
 
+import cats.implicits._
 import io.circe._
 
 final case class Label private (val value: String)
@@ -8,7 +9,7 @@ object Label {
   def unsafe(value: String): Label = new Label(value)
 
   def parse(value: String): Label = 
-    if (value.startsWith("@")) unsafe(value) else unsafe("@" + value)
+    unsafe("@" + value.dropWhile(_ === '@'))
 
   given Decoder[Label] = Decoder[String].map(parse)
   given Encoder[Label] = Encoder[String].contramap[Label](_.value)
